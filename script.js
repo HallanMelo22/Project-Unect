7ea7c74b9d968c15dadbe9c8b3b8fbc859b7804c
HallanMelo22 <hallanfer@gmail.com>
2023-05-15 17:19:10 -0300

    commit 4

--- script.js
+++ script.js
@@ -21,12 +21,12 @@ function novatask() {
 
   const afazer = document.getElementById("cinza1");
   const tarefa = document.createElement("li");
-  tarefa.innerHTML = `<li id="box" class="nodescription">
+  tarefa.innerHTML = `<li id="box">
   <h1><strong>${val_inp1}<strong></h1>
   <a id="more_vert"><span class="material-icons-outlined">
 						more_vert
 					</span></a>
-          <button id="exp-det" onclick="showdetalhes()">
+          <button id="expdet" >
           <h2 id="l-desc">
 				<en>Ler descrição</en>
 			  </h2> 
@@ -53,34 +53,38 @@ function novatask() {
   input1.value = "";
   input2.value = "";
 }
-//const buttondesc = document.getElementById("exp-det");
 
-//buttondesc.addEventListener("click", showdetalhes);
-
-const showdetalhes = () => {
+function togglebox() {
   const box = document.getElementById("box");
-  const ldesc = document.getElementById("l-desc");
-  const edesc = document.getElementById("e-desc");
-  const exp_less = document.getElementById("exp_less");
-  const expand_more = document.getElementById("expand_more");
-  const detail = document.getElementById("desc");
-  const update_button = document.getElementById("update-button");
+  box.classList.toggle("description");
+}
+const element = document.getElementById("expdet");
+element.addEventListener("click", eliminatedesc());
 
-  if (box.className == "nodescription") {
-    box.className = "description";
-    ldesc.className = "description";
-    edesc.className = "description";
-    exp_less.className = "description";
-    expand_more.className = "description";
-    detail.className = "description";
-    update_button.className = "description";
-  } else {
-    box.className = "nodescription";
-    ldesc.className = "nodescription";
-    edesc.className = "nodescription";
-    exp_less.className = "nodescription";
-    expand_more.className = "nodescription";
-    detail.className = "nodescription";
-    update_button.className = "nodescription";
+function eliminatedesc() {
+  localStorage.removeItem("description");
+  togglebox();
+  if (box.classList.contains("description")) {
+    localStorage.toggle("description");
   }
-};
+}
+function togglebox() {
+  const box = document.getElementById("box");
+  box.classList.toggle("description");
+}
+
+function toggledark() {
+  var algo = document.getElementsByTagName("body");
+  algo.classList.add("dark");
+}
+
+function changetheme() {
+  console.log("oi");
+  localStorage.removeItem("dark");
+  toggledark();
+  var algo = document.getElementById("body");
+
+  if (algo.classList.contains("dark")) {
+    localStorage.classList.add("dark");
+  }
+}
